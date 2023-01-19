ac8842218db390aba8b063c4eb42253ed52c94db
Tudor Huza <huzatudor@gmail.com>
2023-01-19 19:56:33 +0200

    Add project files.

---
+++ Emulator ARC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emulator_ARC {
+	internal class Program {
+		static void Main(string[] args) {
+		}
+	}
+}
